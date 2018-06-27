45ee0d3835cc27db2754ed193f9edac25d86cd9c
Noelle <noelle.teh@gmail.com>
2018-06-27 15:20:15 -0700

    added css

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-$("h1").click(function() {
-    alert("clicked");
+$("button").hover(function() {
+    $("#wrapper").css("background-image", `url("beach.jpg")`);
+},
+function() {
+    $("#wrapper").css("background", "black");
+});
+
+$("button").click(function() {
+    console.log("turning off");
+    $("button").off("mouseenter mouseleave");
 })
